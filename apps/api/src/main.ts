6764b8ebf2bfeab14d633856125d49f239d18d81
okenn <azzkentanho@gmail.com>
2026-06-17 11:38:01 +0500

    Fix API CORS for Vercel

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,8 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      "http://localhost:3000",
+      "https://qanot-ashy.vercel.app",
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   });
 
   await app.listen(4000);
